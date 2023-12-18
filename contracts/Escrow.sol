147570ac793f71f226fdbb5a30721f783ae3075a
Cyrus <cyrusyong1@gmail.com>
2023-12-17 23:38:36 -0500

    rudimentary escrow contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "hardhat/console.sol";
-
+import 'hardhat/console.sol';
 contract Escrow {
     address public depositor;
     address public beneficiary;
