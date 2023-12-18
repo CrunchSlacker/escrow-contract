3e2f82cbe4470964a53065ae91ad9121c42381f5
Cyrus <cyrusyong1@gmail.com>
2023-12-17 23:37:53 -0500

    script that deploys to sepolia testnet

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+const ethers = require("hardhat");
+
+async function main() {
+  const escrow = await hre.ethers.deployContract("Escrow", ["0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"]);
+  await escrow.waitForDeployment();
+
+  console.log("Contract Deployed to: ", escrow.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
