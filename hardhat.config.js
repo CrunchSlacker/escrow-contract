4783128974a7450a86edee7155fec992099e9c9e
Cyrus <cyrusyong1@gmail.com>
2023-12-17 23:39:06 -0500

    added sepolia testnet to the network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   networks: {
+    hardhat: {},
     localhost: {
       url: "http://127.0.0.1:8545",
       mining: {
         auto: false,
         interval: 1000,
       },
+    },
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 11155111,
     },
   },
 };
