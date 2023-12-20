2e90633cdb7cc6e21083f23d58fbffc59222697f
Cyrus <cyrusyong1@gmail.com>
2023-12-20 17:33:12 -0500

    minor edits

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  const escrow = await hre.ethers.deployContract("Escrow", ["0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"]);
+  const amount = hre.ethers.parseEther("0.01");
+  const escrow = await hre.ethers.deployContract(
+    "Escrow",
+    [
+      "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+      "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
+    ],
+    { value: amount }
+  );
   await escrow.waitForDeployment();
 
   console.log("Contract Deployed to: ", escrow.target);
