6b348903b2b7f2ba40c080664e9b9c48191dc556
Cyrus <cyrusyong1@gmail.com>
2023-12-20 17:28:51 -0500

    added receive function

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,33 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import 'hardhat/console.sol';
+import "hardhat/console.sol";
+
 contract Escrow {
     address public depositor;
     address public beneficiary;
     address public arbiter;
-    uint public balance;
 
     constructor(address _beneficiary, address _arbiter) payable {
         depositor = msg.sender;
         beneficiary = _beneficiary;
         arbiter = _arbiter;
-        balance = msg.value;
     }
 
     function approve() external onlyArbiter {
-        emit Approved(balance);
+        emit Approved(address(this).balance);
 
-        (bool success, ) = beneficiary.call{value: balance}("");
+        (bool success, ) = beneficiary.call{value: address(this).balance}("");
         require(success);
-
-        balance = address(this).balance;
     }
 
     modifier onlyArbiter() {
         require(msg.sender == arbiter, "You're not the arbiter");
         _;
     }
+
+    receive() external payable {}
 
     event Approved(uint sent);
 }
